dff6b55707471cdcb83ad89143e164bef9d85f06
Ayan Khandelwal <khandelwalayan11@gmail.com>
2025-04-20 00:25:19 +0530

    Add UserName to post models and controller

--- backend/models/Reply.js
+++ backend/models/Reply.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 
 const ReplySchema = new mongoose.Schema({
   user: { type: mongoose.Schema.Types.ObjectId, ref: 'UserAuth', required: true },
+  userFullName: { type: String, required: true },
   comment: { type: mongoose.Schema.Types.ObjectId, ref: 'Comment', required: true },
   text: { type: String, required: true, maxlength: 500 },
   likes: { type: Number, default: 0 }
