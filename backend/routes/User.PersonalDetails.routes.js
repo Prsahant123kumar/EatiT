34fafa4ec6ae360688f084e06ceabae3cd7d1b78
ROZGAR <kumarparshant12540@gmail.com>
2025-04-19 23:16:17 +0530

    Update User.PersonalDetails.routes.js

--- backend/routes/User.PersonalDetails.routes.js
+++ backend/routes/User.PersonalDetails.routes.js
@@ -1,9 +1,9 @@
 const express = require("express");
-const  { EnterPersonaldetails , updateProfile} = require("../controllers/PersonalDetails.controller");
+const  { EnterPersonaldetails , updateProfile , FetchDetails} = require("../controllers/PersonalDetails.controller");
 const { isAuthenticated } = require("../middlewares/isAuthenticated");
-
+const upload = require("../middlewares/multer.js");
 const router = express.Router();
-
-router.route("/Enter-Personal-Details").post(isAuthenticated, EnterPersonaldetails);
-router.route("/Enter-Personal-Details").put(isAuthenticated,updateProfile);
+router.post("/Enter-Personal-Details", isAuthenticated, upload.single("image"), EnterPersonaldetails);
+router.route("/Update-Personal-Details").put(isAuthenticated,updateProfile);
+router.route("/me").get(isAuthenticated,FetchDetails);
 module.exports = router;
